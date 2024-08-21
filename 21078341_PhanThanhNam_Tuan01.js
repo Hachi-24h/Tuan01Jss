db8c5846d610b9782a3d5f4cc5a295d459e1e156
HACHI <shinnichi987@gmail.com>
2024-08-21 10:39:47 +0700

    commit bai 05 phan 02

--- 21078341_PhanThanhNam_Tuan01.js
+++ 21078341_PhanThanhNam_Tuan01.js
@@ -283,3 +283,24 @@ const tbTotal = calcAverage(totals);
 
 console.log('Average Tip: ', tbTip);
 console.log('Average Total: ', tbTotal);
+
+
+// Phần 2 - bài 5
+
+console.log('\n--------------------Phần 2 - bài 5---------------------');
+function printForecast(arr) {
+    let str = '';
+    for (let i = 0; i < arr.length; i++) {
+        str += `${arr[i]}°C in ${i + 1} days ... `;
+    }
+
+    console.log(str);
+}
+
+const data1 = [17, 21, 23];
+const data2 = [12, 5, -5, 0, 4];
+
+console.log('----FORECAST 1----');
+printForecast(data1);
+console.log('----FORECAST 2----');
+printForecast(data2);
